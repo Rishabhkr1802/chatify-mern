f2e55fac22aae2a80de3ab41a7f33aba1b13f647
Rishabh Kumar <krrishabh1802@gmail.com>
2025-10-25 14:58:07 +0530

    Update basic configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,8 +9,6 @@ dotenv.config({
 const port  = process.env.PORT || 3000;
 const app   = express();
 
-app.express();
-
 app.listen(() => {
     console.log(`Server is running at ${port}`);
     connectDB();
