586bb97a22a94b26239f52e3ece35770d0b1a69c
Rishabh Kumar <krrishabh1802@gmail.com>
2025-10-28 13:04:03 +0530

    Configure tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div class="flex flex-col items-center p-7 rounded-2xl bg-slate-900 h-screen">
+      <div class="flex">
+        <span>Class Warfare</span>
+        <span>The Anti-Patterns</span>
+        <span class="flex">
+          <span>No. 4</span>
+          <span>·</span>
+          <span>2025</span>
+        </span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
