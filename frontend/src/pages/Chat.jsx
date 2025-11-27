97b219a5aaab511b902a24aec2bdc92ffe47eefa
Rishabhkr1802 <krrishabh1802@gmail.com>
2025-11-27 22:18:57 +0530

    Fix bug

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -10,7 +10,11 @@ import MessageInput from "../sharedComponents/MessageInput";
 function Chat() {
   const { id: userToChatID }    = useParams();
   const dispatch                = useDispatch();
-  const { isLoading } = useSelector((state) => state.messages);
+  const { isLoading }           = useSelector((state) => state.messages);
+
+  const { user } = useSelector((state) => state.auth);
+  // const { users } = useSelector((state) => state.users);
+  const { messages } = useSelector((state) => state.messages);
 
   useEffect(() => {
     dispatch(getMessage(userToChatID));
@@ -20,7 +24,7 @@ function Chat() {
     <>
       <Container heading={"Chat"} footer={<MessageInput />}>
         {isLoading && <MessageSkelaton />}
-        {!isLoading && <Messages />}
+        {!isLoading && <Messages messages={messages} user={user}/>}
       </Container>
     </>
   )
--- frontend/src/sharedComponents/Messages.jsx
+++ frontend/src/sharedComponents/Messages.jsx
@@ -1,59 +1,23 @@
-import { useSelector } from "react-redux";
-
-function Messages() {
-  const { user } = useSelector((state) => state.auth);
-  const { messages, isLoading } = useSelector((state) => state.messages);
-  console.log(messages)
-
+function Messages({messages, user}) {
   return (
     <>
-      {/* <div className="flex-1 px-6 overflow-y-auto py-8">
-        {messages &&  messages.length > 0  ? (
-          <div className="max-w-3xl mx-auto space-y-6">
-            {messages.map((msg) => (
-              <div key={msg._id} className={`chat ${msg.senderId === user._id ? "chat-end" : "chat-start"}`}>
-                <div  className={`chat-bubble relative ${msg.senderId === user._id
-                      ? "bg-cyan-600 text-white"
-                      : "bg-slate-800 text-slate-200"
-                    }`}
-                >
-                  {msg.image && (
-                    <img src={msg.image} alt="Shared" className="rounded-lg h-48 object-cover" />
-                  )}
-                  {msg.text && <p className="mt-2">{msg.text}</p>}
-                  <p className="text-xs mt-1 opacity-75 flex items-center gap-1">
-                    {new Date(msg.createdAt).toLocaleTimeString(undefined, {
-                      hour: "2-digit",
-                      minute: "2-digit",
-                    })}
-                  </p>
-                </div>
-              </div>
-            ))}
-            <div />
-          </div>
-        ) : "isMessagesLoading" ? (
-          ""
-        ) : (
-          "https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
-        )}
-      </div> */}
-
       {messages.map((msg) => (
+
         <div key={msg._id} className={`chat ${msg.receiverID === user?._id ? "chat-start" : "chat-end"}`}>
           <div className="chat-image avatar">
             <div className="w-10 rounded-full">
-              <img src={msg.receiverID === user._id ? "" : user.profilePic}  alt="Tailwind CSS chat bubble component" />
-              {/* // <img src={`${sg.senderId === user._id} ? ${user.profilePic}: ""`}  alt="Tailwind CSS chat bubble component" /> */}
-              {/* <img src={user.profilePic}  alt="Tailwind CSS chat bubble component" /> */}
+              <img src={msg.receiverID === user._id ? "" : user.profilePic} alt="user" />
             </div>
           </div>
           <div className="chat-header">
             {msg.senderId}
-            <time className="chat-footer opacity-50">{new Date(msg.createdAt).toLocaleTimeString()}</time>
+            <time className="chat-footer opacity-50">{new Date(msg.createdAt).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}</time>
           </div>
-          <div className="chat-bubble">{msg.text}</div>
-            <time className="text-xs opacity-50">{'sent'}</time>
+          {msg.image && (
+            <img src={msg.image} alt="Shared" className="rounded-lg h-48 object-cover" />
+          )}
+          <div className="chat-bubble relative">{msg.text}</div>
+          <time className="text-xs opacity-50">{'sent'}</time>
         </div>
       ))}
     </>
