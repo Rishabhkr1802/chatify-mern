7c50e4a46fe136b6ba90aa068ec4e7e47d3a2301
Rishabhkr1802 <krrishabh1802@gmail.com>
2025-12-07 22:58:48 +0530

    Update deployement config

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,9 @@ import userRoutes       from "./routes/user.route.js";
 
 import { app, server } from "./socket/socketio.js";
 
+import path              from "path";   // For Deployment
+import { fileURLToPath } from 'url';   // For Deployment
+
 dotenv.config({
     path: "./.env",
 });
@@ -17,19 +20,31 @@ dotenv.config({
 const port  = process.env.PORT || 3000;
 // const app   = express();
 
+const __filename = fileURLToPath(import.meta.url);  // For Deployment
+const __dirname  = path.dirname(__filename);        // For Deployment
+// const __dirname = path.resolve();              // For Deployment
+
 app.use(cors({
     origin: ['http://localhost:3000','http://192.168.0.102:3000'],
     credentials: true,
 }));
 
 app.use(express.json()) // {limit: '20kb'} For Accept form input
-app.use(express.urlencoded({extended: 'false', limit: '20kb'})) //For Accept data from URL like id,queryParams etc
+app.use(express.urlencoded({extended: false, limit: '20kb'})) //For Accept data from URL like id,queryParams etc
 app.use(express.static("uploads"));
 app.use(cookieparser());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/message", messageRoutes);
 app.use("/api/v1/user",userRoutes);
+
+// For Deployment
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+    app.get("/*splat", (req, res) => {
+        res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+    })
+}
 
 server.listen(port, () => {
     console.log(`Server is running at ${port}`);
--- backend/src/socket/socketio.js
+++ backend/src/socket/socketio.js
@@ -7,7 +7,8 @@ const server  = http.createServer(app);
 const io      = new Server(server, {
   cors: {
     origin  : ["http://localhost:3000"],
-    methods : ['GET', 'POST']
+    methods : ['GET', 'POST'],
+    pingTimeout: 60000,
   }
 });
 const userSocketMap = {}; // {userId: socketId}
